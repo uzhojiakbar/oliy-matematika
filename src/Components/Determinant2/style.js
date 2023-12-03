1465acd690907e724704544c02458ecf6989abae
uzhojiakbar3 <uzhojiakbar3@gmail.com>
2023-12-03 11:36:23 +0500

    Determinant re-design

--- src/Components/Determinant2/style.js
+++ src/Components/Determinant2/style.js
@@ -1,45 +1,62 @@
 import styled from "styled-components";
 
 const ContainerDet = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  gap: 20px;
-
-  padding: 10px 0;
-  .determinant {
+  .section {
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    overflow: auto;
 
-    .input {
-      display: grid;
-      grid-template-columns: repeat(2, 60px);
+    gap: 20px;
 
-      padding: 5px 10px;
-      width: fit-content;
-      gap: 10px;
-      input {
-        border: none;
-        outline: none;
-
-        background-color: rgb(66, 63, 63);
-        border: 1px solid rgba(255, 255, 255, 0.5);
-
-        min-width: 40px;
-
-        color: white;
-
-        padding: 5px 2px;
-      }
-    }
-    .output {
+    border-left: 2px solid rgba(255, 255, 255, 0.5);
+    border-right: 2px solid rgba(255, 255, 255, 0.5);
+    /* border-radius: 15px; */
+    padding: 10px;
+    .determinant {
       display: flex;
       align-items: center;
+      .input {
+        display: grid;
+        grid-template-columns: repeat(2, 60px);
+
+        padding: 5px 10px;
+        width: fit-content;
+        gap: 10px;
+        input {
+          border: none;
+          outline: none;
+
+          background-color: rgb(66, 63, 63);
+          border: 1px solid rgba(255, 255, 255, 0.5);
+
+          min-width: 40px;
+
+          color: white;
+
+          padding: 5px 2px;
+        }
+      }
+      .output {
+        display: flex;
+        align-items: center;
+        padding: 0 10px;
+
+        .javob {
+          display: flex;
+          align-items: center;
+
+          gap: 5px;
+        }
+      }
     }
-  }
-  button {
-    color: black;
-    padding: 0 5px;
+    .option {
+      display: flex;
+      gap: 10px;
+      button {
+        color: black;
+        padding: 0 5px;
+      }
+    }
   }
 `;
 
